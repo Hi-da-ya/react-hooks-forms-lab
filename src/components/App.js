649444108b5bb9afe70c9a18d08bec51791392f7
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-05-15 15:23:50 +0300

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,14 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  //function that updates item state and adds a new element to the list
+  function updateItemState(addedItem){
+    setItems([...items, addedItem])
+  };
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={updateItemState}/>
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+
+  const [itemName, setItemName] = useState("")
+  const [itemCategory, setItemCategory] = useState("Produce")
+
+  const newItem = {
+    id: uuid(),
+    name: itemName,
+    category: itemCategory,
+  };
+
+  //function that handles input change for name
+
+  function handleNameChange(event){
+    setItemName(event.target.value)
+  };
+
+  //function that handles input change for category
+  function handleCategoryChange(event){
+    setItemCategory(event.target.value)
+  };
+
+  //function that handles form submission
+  function handleSubmit(event){
+    event.preventDefault()
+   onItemFormSubmit(newItem)
+  };
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={itemName} onChange={handleNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={itemCategory} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
